b7b7a728bd1644f1b698559f729b9bfce70ed879
mateusdaemon <mateusjdm@gmail.com>
2024-10-02 23:27:03 -0300

    importing joystick

--- JurassicJump/Assets/Scripts/Player.cs
+++ JurassicJump/Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 10;
+    
+    private FloatingJoystick joystick;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        joystick = FindObjectOfType<FloatingJoystick>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(joystick.Direction);
+        rb.velocity = joystick.Direction * speed;
     }
 }
